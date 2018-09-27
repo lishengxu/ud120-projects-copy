ef51138203f2d1902b48c8ac5ac49416edd1e2ec
lishengxu <lsx1@meitu.com>
2018-09-27 11:19:48 +0800

    add tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -21,11 +21,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "tree fit time:", round(time()-t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "tree pred time:", round(time()-t0, 3), "s"
+
+print "score is ", clf.score(features_test, labels_test)
+
+from sklearn.metrics import accuracy_score
+print "acuracy score is ", accuracy_score(pred, labels_test)
+
+print "features number is ", len(features_train[0])
 #########################################################
 
 
