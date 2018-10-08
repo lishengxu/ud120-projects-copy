f0f1bc317d5160c8405b5b465e8c5e2bce2b21a0
lishengxu <lsx1@meitu.com>
2018-10-08 13:31:22 +0800

    add salary test

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -50,3 +50,48 @@ for key in enron_data.keys():
     if key.lower().find("Jeffrey".lower()) >= 0 and \
             key.lower().find("Skilling".lower()) >= 0:
         print enron_data.get(key).get("exercised_stock_options")
+
+max_total_payments = 0
+max_total_payments_person = ""
+for key in enron_data.keys():
+    low = key.lower()
+    if low == "lay kenneth l" or low == "skilling jeffrey k" or low == "fastow andrew s":
+        cur_payments = enron_data.get(key).get("total_payments")
+        if max_total_payments < cur_payments:
+            max_total_payments = cur_payments
+            max_total_payments_person = key
+print max_total_payments_person, " has max_total_payments ", max_total_payments
+
+has_salary_count = 0
+for key in enron_data.keys():
+    if enron_data.get(key).get("salary") != "NaN":
+        has_salary_count += 1
+print "has salary count:", has_salary_count
+
+has_email_add = 0
+for key in enron_data.keys():
+    if enron_data.get(key).get("email_address") != "NaN":
+        has_email_add += 1
+print "has email add:", has_email_add
+
+has_not_total_payments = 0
+total_count = 0
+for key in enron_data.keys():
+    total_count += 1
+    if enron_data.get(key).get("total_payments") == "NaN":
+        has_not_total_payments += 1
+print "has not total payments:", has_not_total_payments, "percents:", has_not_total_payments * 1.0 / total_count
+
+has_not_total_payments = 0
+total_count = 0
+for key in enron_data.keys():
+    total_count += 1
+    if enron_data.get(key).get("poi") and enron_data.get(key).get("total_payments") == "NaN":
+        has_not_total_payments += 1
+
+print "poi has not total payments:", has_not_total_payments, "percents:", has_not_total_payments * 1.0 / total_count
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat
+
+#print featureFormat(enron_data, list(enron_data.get(tuple(enron_data.keys())[0])))
